10f6c15876ed25c8bf225b7f86b69e8906fc2a50
Anton Shevchuk <AntonShevchuk@gmail.com>
2017-03-28 12:11:37 +0300

    Fixed #2 - Fixed structure Fixed #3 - Fixed migrations

---
+++ data/migrations/20161206160421_options.php
@@ -0,0 +1,50 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateOptionsTable extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $pages = $this->table('options', ['id' => false, 'primary_key' => ['namespace', 'key']]);
+        $pages
+            ->addColumn('namespace', 'string', ['length' => 255, 'default' => 'default'])
+            ->addColumn('key', 'string', ['length' => 255])
+            ->addColumn('description', 'text')
+            ->addTimestamps('created', 'updated')
+            ->create();
+
+        $data = [
+            [
+                'roleId' => 2,
+                'module' => 'options',
+                'privilege' => 'Management'
+            ]
+        ];
+
+        $privileges = $this->table('acl_privileges');
+        $privileges->insert($data)
+            ->save();
+    }
+}
